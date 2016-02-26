8eb0a21e0fe84995672c3df1f01648d728752ce7
Robert Horlings <robert@isdat.nl>
2016-02-26 11:46:47 +0100

    Added functionality to link to other page from tooltip.
    Please note that this functionality only works on mobile devices. On
    non-mobile devices, when the mouse hovers the popup, it will start to
    disappear immediately, not allowing the user to click the popup.
    This is caused by the pointer-events: all setting in css, which is
    needed to enable the onclick event in the tooltip anyhow.

--- src/charts.js
+++ src/charts.js
@@ -1130,7 +1130,15 @@
 
       //initialize the tooltip
       var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
-        return '<div class="omh-tooltip" data-datapoint-id="' + d.omhDatum.header.id + '">' + getTipContent( d, measureList ) + '</div>';
+        var div = '<div class="omh-tooltip" data-datapoint-id="' + d.omhDatum.header.id + '" ';
+
+        if( interfaceSettings.tooltips.link ) {
+          var url = interfaceSettings.tooltips.link.replace( '{measurementId}', d.omhDatum.header.id );
+          div += ' onclick="location.href=\'' + url + '\'; return false;" ';
+        }
+
+        div += '>' + getTipContent( d, measureList ) + '</div>';
+        return div;
       });
 
     }
